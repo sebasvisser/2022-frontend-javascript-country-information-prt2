e2f50ac5ec0b43fe6b64a537fe9dd71df47ef981
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-16 11:02:17 +0100

    currency en flag nu ook werkend

--- src/search.js
+++ src/search.js
@@ -12,35 +12,47 @@ async function getCountryDataOnSearch() {
         const searchQuery = document.getElementById("searchcountry").value;
 
         const response = await axios.get(`https://restcountries.com/v2/name/${searchQuery}`);
-        const countries = response.data;
+        const countries = response.data[0];
 
+        console.log(countries);
         //resultaat data specificeren
         const nameCountry = countries.name;
-        const flagCountry = countries.flag;
+        const flagCountry = countries.flags.png;
         const subareaCountry = countries.subregion;
         const populationCountry = countries.population;
         const capitalCountry = countries.capital;
-        // const currenciesCountry = countries.currencies[0].name;
+        let currenciesCountry = "";
+        // currencies ophalen
+        if (countries.currencies.length === 1) {
+            currenciesCountry = countries.currencies[0].name + "'s";
+        }else {
+            for (let i = 0; i < countries.currencies.length ; i++) {
+                currenciesCountry += countries.currencies[i].name;
+                currenciesCountry += "'s and "
+            }
+            // laatste and weghalen
+            currenciesCountry = currenciesCountry.substring(0, currenciesCountry.length - 5);
+        }
 
         // resultaat blokje samenstellen
         const item = document.createElement('li');
 // vlag
-        const countryFlag = document.createElement("img");
-        countryFlag.setAttribute("alt", nameCountry + "_flag");
-        countryFlag.setAttribute("src",flagCountry);
-        countryFlag.setAttribute("class", "flag");
+        const searchCountryFlag = document.createElement("img");
+        searchCountryFlag.setAttribute("alt", nameCountry + "_flag");
+        searchCountryFlag.setAttribute("src",flagCountry);
+        searchCountryFlag.setAttribute("class", "flag");
 // naam van land
         const countryName = document.createElement("span");
         countryName.textContent = nameCountry;
 // zinnetje
         const countryPopulation = document.createElement("p");
-        countryPopulation.textContent = nameCountry +  " is situated in " + subareaCountry + ". It has a population of " + populationCountry + ". The capital is " + capitalCountry + "and you can pay with CURRENCY ";
+        countryPopulation.textContent = `${nameCountry} is situated in ${subareaCountry}. It has a population of ${populationCountry}. The capital is ${capitalCountry} and you can pay with ${currenciesCountry}.`;
 
         // alles plaatsen op de plek van results
         result.append(item);
-        item.append(countryFlag);
+        item.append(searchCountryFlag);
         item.append(countryName);
-        item.append("countryPopulation");
+        item.append(countryPopulation);
     }
     catch (error) {
         console.error(error);
